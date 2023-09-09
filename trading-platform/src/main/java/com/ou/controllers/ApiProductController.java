b424511e1195686fb801a4df09f176b8dc1bcf8d
“[firstname <phannhuttan2002@gmail.com>
2023-09-09 16:10:28 +0700

    update

--- trading-platform/src/main/java/com/ou/controllers/ApiProductController.java
+++ trading-platform/src/main/java/com/ou/controllers/ApiProductController.java
@@ -129,6 +129,22 @@ public class ApiProductController {
 
     }
 
+    //lấy tất cả prodcut của thg đăng nhập
+    @GetMapping("/products/")
+    @CrossOrigin
+    public ResponseEntity<List<Products>> getProduct() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && !(authentication instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Users userCuren = userSer.getUsers(userDetails.getUsername());
+            Store store = this.storeService.getStoreByUserID(userCuren);
+            return new ResponseEntity<>(this.ProductService.getProduct(store), HttpStatus.OK);
+        }
+        return null;
+
+    }
+
     //lấy tất cả prodcut
     @GetMapping("/allProducts/")
     @CrossOrigin
